8d77521fe43f0f8a919984c526fa7508d02d9ef4
sgarcia@innovahn.com <sgarcia@innovahn.com>
2015-03-26 16:59:52 -0600

    Agregado los archivos de sustitucion realizados por Elmer

--- InnovaBase/models/AdministradorDeCambios.py
+++ InnovaBase/models/AdministradorDeCambios.py
@@ -4,29 +4,27 @@ import shutil
 import time
 
 class administradorcambios(osv.Model):
-    	_name = 'innovabase.administradorcambios'
+    _name = 'innovabase.administradorcambios'
 	_columns = {
-        'path': fields.text('path'),
+    'path': fields.text('path'),
 	'active': fields.boolean('activo'),
-    	}
+    }
 
 	def create(self, cr, uid, vals, context=None):
 		config_id = super(administradorcambios, self).create(cr, uid, vals, context=context)
 
 		RutaArchivos = vals['path']
-        	infile = open(RutaArchivos+'/configuracion.txt', 'r')
+		infile = open(RutaArchivos+'/configuracion.txt', 'r')
 
-	        # recorremos el archivo linea por linea
-	        for line in infile:
-        	        # ignora cualquier linea que no empiece por el simbolo >
-                	if line.startswith('>'):
-	                        # rutas[1] es el archivo alojado dentro de la carpeta archivos que se desea copiar
-        	                # rutas[2] es la ruta donde deseamos copiar el archivo
-                	        rutas = line.split('>')
-                        	#si se va a sustituir un archivo sacamos un respaldo del archivo
-	                        if os.path.exists(os.getcwd()+'/'+rutas[2].lstrip()[:-1]):
-					print('if'*50)
-					import time
+        # recorremos el archivo linea por linea
+        for line in infile:
+        	# ignora cualquier linea que no empiece por el simbolo >
+        	if line.startswith('>'):
+	    		# rutas[1] es el archivo alojado dentro de la carpeta archivos que se desea copiar
+        		# rutas[2] es la ruta donde deseamos copiar el archivo
+                rutas = line.split('>')
+                #si se va a sustituir un archivo sacamos un respaldo del archivo
+	            if os.path.exists(os.getcwd()+'/'+rutas[2].lstrip()[:-1]):
 					os.rename(os.getcwd()+'/'+rutas[2].lstrip()[:-1],os.getcwd()+'/'+rutas[2].lstrip()[:-1]+time.strftime('%d %b %Y'))
-        	                #copia el archivo rutas[1] en la ubicacion rutas[2]
-                	        shutil.copyfile(RutaArchivos+'/'+rutas[1].lstrip()[:-1],os.getcwd()+'/'+rutas[2].lstrip()[:-1])
+        	    #copia el archivo rutas[1] en la ubicacion rutas[2]
+				shutil.copyfile(RutaArchivos+'/'+rutas[1].lstrip()[:-1],os.getcwd()+'/'+rutas[2].lstrip()[:-1])
