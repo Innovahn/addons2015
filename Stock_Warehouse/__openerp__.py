5895b26362c145df5a4be1519a9585c7ec3a521b
sgarcia@innovahn.com <sgarcia@innovahn.com>
2015-04-06 11:30:03 -0600

    modulo stock

---
+++ Stock_Warehouse/__openerp__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{	
+	'name' : 'Stock Warehouse inh',
+	'author': 'Grupo Innova',
+	'category': 'Stock',
+	'description': """
+	Agrega menus de acceso rápido para el control de entradas y salidas de stock
+	""",
+	'data':['views/stock_menus.xml'],
+	'depends': ['base','stock'],
+   	'installable': True,
+}
